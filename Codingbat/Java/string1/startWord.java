0b17646bfb4bd001a2b727b6892a4a2d99a49a87
91701 <91701@LAPTOP-KFV0PSKR>
2020-05-26 16:37:16 +0530

    Defront and StartWord

---
+++ Codingbat/Java/string1/startWord.java
@@ -0,0 +1,12 @@
+package string1;
+
+public class startWord {
+	public String startWord(String str, String word) {
+	    if(str.length() >= word.length() && 
+	        str.substring(1, word.length()).equals(word.substring(1)))
+	        return str.substring(0, word.length());
+	                  
+	    return "";
+	}
+
+}
